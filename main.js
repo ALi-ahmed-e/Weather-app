f67a7d9d5b0e81d561159de1b77c756d509521e0
ALi-ahmed-e <107031226+ALi-ahmed-e@users.noreply.github.com>
2022-06-16 14:10:21 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 
+
 let inp = document.getElementById('inp')
 let btn = document.getElementById('btn')
 let weather = document.getElementById('weather')
@@ -31,7 +32,9 @@ function red() {
 btn.onclick = (eo) => {
 
 
-  
+  weather.innerHTML = `<div class="spinner-border" style="width: 3rem; height: 3rem; margin-left:140px; margin-top:30px;" role="status">
+  <span class="visually-hidden">Loading...</span>
+</div>`
 
 
   let  api = `https://api.openweathermap.org/data/2.5/weather?q=${inp.value}&units=metric&appid=2833809593787779dafc6f8d072c24e2`
@@ -46,8 +49,8 @@ btn.onclick = (eo) => {
    
     if (data.message !== 'city not found') {
 
-
-      show(data.main.temp, data.name, data.weather[0].main, data.main.humidity, data.wind.speed)
+        console.log(data)
+      show(data.main.temp, data.name, data.weather[0].main, data.main.humidity, data.wind.speed,data.sys.country)
     } else {
       weather.innerHTML = '<h4>Sorry this city is not in database yet</h4>'
     }
@@ -64,11 +67,11 @@ btn.onclick = (eo) => {
 
 get()
 }
-function show(temp, name, clarity, humid, speed) {
+function show(temp, name, clarity, humid, speed,country) {
 
 
   weather.innerHTML = `
- <h2 style="margin-left:20px;">${name}</h2>
+ <h2 style="margin-left:20px; display:inline;">${name}</h2>(${country})
             <h3 style="margin-left:20px;">${temp}°c</h3>
             <div>
                 <span>${clarity}</span>
@@ -80,6 +83,9 @@ function show(temp, name, clarity, humid, speed) {
  `
 
 }
+
+
+
 
 
 
